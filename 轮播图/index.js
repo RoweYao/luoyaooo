489ac8838e779c11dea5edba5318214c34c9da00
luoyaooo <luoyaooo@qq.com>
2020-08-25 20:02:45 +0800

    轮播图添加自动切换，修改底部描述文字窄屏切换效果

--- 轮播图/index.js
+++ 轮播图/index.js
@@ -1,39 +1,71 @@
-// 后退按钮实例
-const prev = document.getElementById('prev')
+// 页面渲染完成后执行js
+window.onload = () => {
+  // 后退按钮实例
+  const prev = document.getElementById('prev')
 
-// 前进按钮实例
-const next = document.getElementById('next')
+  // 前进按钮实例
+  const next = document.getElementById('next')
 
-// 轮播图实例
-const slides = document.getElementsByClassName('slide-item')
+  // 轮播图实例
+  const slides = document.getElementsByClassName('slide-item')
 
-let currentIndex = 0
+  let currentIndex = 0
 
-next.addEventListener('click', handleNext)
-prev.addEventListener('click', handlePrev)
+  // 轮播切换时间
+  let interval = 5000
+  // 是否开启自动播放
+  let autoPlay = true
+  // 图片切换方向 true 下一张 false 上一张
+  let forward = true
+  // 是否切换过图片，用于防止点击后立马自动切换
+  let switchFlag = false
 
-// 下一张图片
-function handleNext () {
-  let current = slides[currentIndex]
-  current.classList.remove('current')
+  next.addEventListener('click', () => {
+    switchFlag = true
+    handleNext()
+  })
+  prev.addEventListener('click', () => {
+    switchFlag = true
+    handlePrev()
+  })
 
-  currentIndex++
-  if (currentIndex >= slides.length) {
-    currentIndex = 0
+  // 定时切换
+  function autoSwith () {
+    if (autoPlay) {
+      setInterval(() => {
+        if (!switchFlag) {
+          forward ? handleNext() : handlePrev()
+        }
+        switchFlag = false
+      }, interval)
+    }
+  }
+  // 下一张图片
+  function handleNext () {
+    let current = slides[currentIndex]
+    current.classList.remove('current')
+
+    currentIndex++
+    if (currentIndex >= slides.length) {
+      currentIndex = 0
+    }
+
+    slides[currentIndex].classList.add('current')
   }
 
-  slides[currentIndex].classList.add('current')
-}
+  // 上一张图片
+  function handlePrev () {
+    let current = slides[currentIndex]
+    current.classList.remove('current')
 
-// 上一张图片
-function handlePrev () {
-  let current = slides[currentIndex]
-  current.classList.remove('current')
+    currentIndex--
+    if (currentIndex < 0) {
+      currentIndex = slides.length - 1
+    }
 
-  currentIndex--
-  if (currentIndex < 0) {
-    currentIndex = slides.length - 1
+    slides[currentIndex].classList.add('current')
   }
 
-  slides[currentIndex].classList.add('current')
+  // 执行自动播放
+  autoSwith()
 }
